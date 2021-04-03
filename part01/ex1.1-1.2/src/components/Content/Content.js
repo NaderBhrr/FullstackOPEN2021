bb95935b8cdc2f4123c3bb3e5625b22fc24c9c62
NaderBhrr <nader.bhr.devv@gmail.com>
2021-04-03 16:06:10 +0430

    :recycle: Refactor the "Content" component to use "Part" component

--- part01/ex1.1-1.2/src/components/Content/Content.js
+++ part01/ex1.1-1.2/src/components/Content/Content.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Content = ({ part, noOfExercises }) => (
+const Part = ({ part, noOfExercises }) => (
   <p>
     {part} {noOfExercises}
   </p>
+);
+
+const Content = (props) => (
+  <>
+    <Part part={props.part1} noOfExercises={props.exercises1} />
+    <Part part={props.part2} noOfExercises={props.exercises2} />
+    <Part part={props.part3} noOfExercises={props.exercises3} />
+  </>
 );
 
 export default Content;
