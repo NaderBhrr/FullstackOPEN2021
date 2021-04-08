5b41ebba3abaf3bd7dfe71b7541ec05f7d749184
NaderBhrr <nader.bhr.devv@gmail.com>
2021-04-08 22:52:51 +0430

    :tada: Initialize the Anecdote app

--- part01/ex1.6-1.14/ex1.12-1.14/src/components/RatingButton/RatingButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './RatingButton.css';
-
-const RatingButton = ({ text, handleRatingsStats }) => (
-  <button onClick={handleRatingsStats}>{text}</button>
-);
-
-export default RatingButton;
