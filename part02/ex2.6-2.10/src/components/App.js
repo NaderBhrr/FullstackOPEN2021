ee246effa5f64684e1a48a3fd2b8567c623d33de
NaderBhrr <nader.bhr.devv@gmail.com>
2021-04-10 15:58:35 +0430

    :zap: Add regexp for invalid entries

--- part02/ex2.6-2.10/src/components/App.js
+++ part02/ex2.6-2.10/src/components/App.js
@@ -2,23 +2,42 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
-
   const [newName, setNewName] = useState('');
 
   const AddNewContact = (event) => {
     event.preventDefault();
+    console.log('new Contact: ', newName);
+
+    // Error handling conditions
+    if (/\W|\d/g.test(newName)) {
+      alert(`${newName} is not a valid entry please try again`);
+      setNewName('');
+      return;
+    }
+    if (
+      !persons.every(
+        (person) => person.name.toLowerCase() !== newName.toLowerCase()
+      )
+    ) {
+      alert(`${newName} already exists in the phonebook`);
+      setNewName('');
+      return;
+    }
+
     setPersons(persons.concat({ name: newName }));
     setNewName('');
   };
 
   const handleNewContact = (event) => {
+    console.log(event.target.value);
     const inputValue = event.target.value;
+    console.log(inputValue === '');
     setNewName(inputValue);
   };
 
   return (
     <div>
-      <div>{(() => console.log(persons, newName))()}</div>
+      {/* <div>{(() => console.log(persons))()}</div> */}
       <h2>Phonebook</h2>
       <form onSubmit={AddNewContact}>
         <div>
