dd2e20b3783868ea0b3b0782eb4cc9f345198a2f
NaderBhrr <nader.bhr.devv@gmail.com>
2021-04-10 17:31:48 +0430

    ✨✨ Add New phone number input with error handling logic

--- part02/ex2.6-2.10/src/components/App.js
+++ part02/ex2.6-2.10/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phoneNumber: '2367492' },
+  ]);
   const [newName, setNewName] = useState('');
+  const [newPhoneNumber, setNewPhoneNumber] = useState('');
 
   const AddNewContact = (event) => {
     event.preventDefault();
@@ -12,6 +15,15 @@ const App = () => {
     if (/\W|\d/g.test(newName)) {
       alert(`${newName} is not a valid entry please try again`);
       setNewName('');
+      setNewPhoneNumber('');
+
+      return;
+    }
+    if (!/\d/g.test(newPhoneNumber)) {
+      alert(`${newPhoneNumber} is not a valid entry please try again`);
+      setNewName('');
+      setNewPhoneNumber('');
+
       return;
     }
     if (
@@ -21,18 +33,26 @@ const App = () => {
     ) {
       alert(`${newName} already exists in the phonebook`);
       setNewName('');
+      setNewPhoneNumber('');
+
       return;
     }
 
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, phoneNumber: newPhoneNumber }));
+
     setNewName('');
+    setNewPhoneNumber('');
   };
 
   const handleNewContact = (event) => {
     console.log(event.target.value);
-    const inputValue = event.target.value;
-    console.log(inputValue === '');
-    setNewName(inputValue);
+    console.log(event.target.value === '');
+    setNewName(event.target.value);
+  };
+
+  const handleNewPhoneNumber = (event) => {
+    console.log(event.target.value);
+    setNewPhoneNumber(event.target.value);
   };
 
   return (
@@ -45,13 +65,23 @@ const App = () => {
           <input type='text' value={newName} onChange={handleNewContact} />
         </div>
         <div>
+          Contact Number:
+          <input
+            type='text'
+            value={newPhoneNumber}
+            onChange={handleNewPhoneNumber}
+          />
+        </div>
+        <div>
           <button type='submit'>Add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h2>Contact Information:</h2>
       <ul>
         {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+          <li key={index}>
+            {person.name} <div>&nbsp;{person.phoneNumber}</div>{' '}
+          </li>
         ))}
       </ul>
     </div>
