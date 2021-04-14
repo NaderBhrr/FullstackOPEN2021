4cc8985d049b1399408e037fddfaea3de8d50e01
NaderBhrr <nader.bhr.devv@gmail.com>
2021-04-14 10:38:23 +0430

    💄💄 Add the delete button to the "ContactList" component

--- part02/ex2.15-2.18/src/components/ContactsList/ContactList.js
+++ part02/ex2.15-2.18/src/components/ContactsList/ContactList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const ContactList = ({ persons }) => (
+const ContactList = ({ persons, deleteContact }) => (
   <div>
     <h2>Contact Information:</h2>
     <ul>
       {persons.map((person, index) => (
         <li key={index}>
           {person.name} <div>&nbsp;{person.number}</div>{' '}
+          <button onClick={deleteContact}>Delete</button>
         </li>
       ))}
     </ul>
