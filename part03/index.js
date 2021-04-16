3abb1325c066eb354912e63379e8cc87a7b67f3f
NaderBhrr <nader.bhr.devv@gmail.com>
2021-04-17 00:04:20 +0430

    ✨✨ Add endpoint for single phonebook entry for a single contact

--- part03/index.js
+++ part03/index.js
@@ -6,12 +6,28 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api/persons', (_req, res) => {
-  cat('./db.json').then((notes) => res.json(notes));
+  cat('./db.json').then((persons) => res.json(persons));
 });
 
-app.get('/info', (req, res) => {
-  cat('./db.json').then((notes) => {
-    const info = `Phonebook has information for ${notes.length} people`;
+const respondNoContact = (message) =>
+  `<header>
+    <h3>${message}</h3>
+  </header>`;
+
+app.get('/api/persons/:id', (req, res) => {
+  const { id } = req.params;
+  cat('./db.json').then((persons) => {
+    const person = persons.find((person) => person.id === Number(id));
+
+    person
+      ? res.json(person)
+      : res.status(422).end(respondNoContact('No Contact Found'));
+  });
+});
+
+app.get('/info', (_req, res) => {
+  cat('./db.json').then((persons) => {
+    const info = `Phonebook has information for ${persons.length} people`;
     const date = new Date();
 
     res.end(`${info}\n${date}`);
