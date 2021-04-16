4bc7b301004d29c94ef8f8817f62f03270ca8fe4
NaderBhrr <nader.bhr.devv@gmail.com>
2021-04-16 23:47:24 +0430

    ✨✨ Add /info endpoint to the routes

--- part03/index.js
+++ part03/index.js
@@ -9,6 +9,15 @@ app.get('/api/persons', (_req, res) => {
   cat('./db.json').then((notes) => res.json(notes));
 });
 
+app.get('/info', (req, res) => {
+  cat('./db.json').then((notes) => {
+    const info = `Phonebook has information for ${notes.length} people`;
+    const date = new Date();
+
+    res.end(`${info}\n${date}`);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server started successfully on port: http://localhost:${PORT}`);
 });
